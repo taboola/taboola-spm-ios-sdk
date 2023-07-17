fb3b0a06c188d10a8b10e391ff72e0dc19219eaa
Roman Slyepko <roman.slyepko@gmail.com>
2023-07-17 09:51:27 +0300

    update TaboolaSDK to 3.8.11

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/spm/3.8.5/TaboolaSDK.xcframework.zip",
-                      checksum: "f24e73af03c51a6ddb387a45e846b2a53d9e2314a3f690c5e1854f97d6a2a187")
+                      url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/spm/3.8.11/TaboolaSDK.xcframework.zip",
+                      checksum: "7e689fccd3748107043ec429bb86801f88566db03027cbe66a7fe69374c48bd9")
     ]
 )
