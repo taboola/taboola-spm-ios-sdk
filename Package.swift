4da51b612ba804567e3e778a49fbdb8fbc1c554d
Yoni Vizel <yoni.v@taboola.com>
2023-12-25 11:58:46 +0200

    MOB-2392 Updated Package to temporary XCFramework SDK with swift which calls to ObjC (with private modulemap)

--- Package.swift
+++ Package.swift
@@ -12,8 +12,11 @@ let package = Package(
         )
     ],
     targets: [
+//        .binaryTarget(name: "TaboolaSDK",
+//                      url: "https://dl.dropbox.com/scl/fi/p41zxtmdj5tzbl1i7acar/TaboolaSDK.xcframework.zip?rlkey=hc9u9pqgx1gny0mytaho2107f&dl=0",
+//                      checksum: "850def80835d91a5e625c990eda8c1052b32581c1a0c82019e734a9795626851")
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://dl.dropbox.com/scl/fi/p41zxtmdj5tzbl1i7acar/TaboolaSDK.xcframework.zip?rlkey=hc9u9pqgx1gny0mytaho2107f&dl=0",
-                      checksum: "850def80835d91a5e625c990eda8c1052b32581c1a0c82019e734a9795626851")
+                      url: "https://www.dropbox.com/scl/fi/tq2dwmpkotmqcrv6u9o9p/TaboolaSDK.xcframework.zip?rlkey=7rcqyah2q0mz9ucjfh6y3krsl&dl=0",
+                      checksum: "5a2a2d1e2062879646343b16d48d7a7ca9643ac946cee482bd12476975f95291")
     ]
 )
