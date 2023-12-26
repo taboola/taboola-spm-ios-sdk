acd8e1eee563cfaef5ebe799cd1dd79dc037c0e7
Yoni Vizel <yoni.v@taboola.com>
2023-12-26 10:22:11 +0200

    2rd try

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 //                      url: "https://dl.dropbox.com/scl/fi/p41zxtmdj5tzbl1i7acar/TaboolaSDK.xcframework.zip?rlkey=hc9u9pqgx1gny0mytaho2107f&dl=0",
 //                      checksum: "850def80835d91a5e625c990eda8c1052b32581c1a0c82019e734a9795626851")
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://www.dropbox.com/scl/fi/tq2dwmpkotmqcrv6u9o9p/TaboolaSDK.xcframework.zip?rlkey=7rcqyah2q0mz9ucjfh6y3krsl&dl=0",
-                      checksum: "5a2a2d1e2062879646343b16d48d7a7ca9643ac946cee482bd12476975f95291")
+                      url: "https://www.dropbox.com/scl/fi/v45h3q62mq2fexczgold5/TaboolaSDK.xcframework.zip?rlkey=9w7124233bor7k0tffpvilr4u&dl=0",
+                      checksum: "d5a92d98b3c8c217886ce3805afe6a5434ad79a1ea2efb17d05247beff28fa79")
     ]
 )
