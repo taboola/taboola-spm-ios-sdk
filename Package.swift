c6d6ea9f34da9e459182fbed915dfde71ad9f96b
Yoni Vizel <yonivav@gmail.com>
2024-02-26 12:23:30 +0200

    MOb-2392 Fix www -> dl (@_impl for SPM)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://www.dropbox.com/scl/fi/mka0crol8nhgj3t8i7q07/TaboolaSDK.xcframework.zip?rlkey=4eu0m7sp0gjjuyirudrofrnw9&dl=0",
+                      url: "https://dl.dropbox.com/scl/fi/mka0crol8nhgj3t8i7q07/TaboolaSDK.xcframework.zip?rlkey=4eu0m7sp0gjjuyirudrofrnw9&dl=0",
                       checksum: "5bcde5808ea1203a0ec4d725927bc53b0a95906d767d6dfd4bc7e124cd542020")
     ]
 )
