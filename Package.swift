44c74dd6147bc252febd4143a8ed6d42ad4be1da
build <build@mini-mac-stadium001.local>
2024-05-15 15:31:09 +0100

    Update Package to version 3.8.27-beta-2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/official/2.9.3/TaboolaSDK.xcframework.zip",
-                      checksum: "f0350a8cfd7d36720adf75b3e93dbc9886aa47f28d793ad195bed112e0df9e60")
+                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/3.8.27/beta-2/TaboolaSDK.xcframework.zip",
+                      checksum: "654849c343b14b5132ce219b1e613ad04f0715b5ea39be9b2d21502b44b99d66")
     ]
 )
