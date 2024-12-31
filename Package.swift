623cfb18130e34feecebcb8479f4f946f4f7c5c2
builder <builder@taboola.com>
2024-12-31 15:59:21 +0000

    Update Package to version 4.0.4-beta-18

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/4.0.4/beta-9/TaboolaSDK.xcframework.zip",
-                      checksum: "8db9605fe2624af7e8b4a256ce1b754f5157c513aeed250abd1b829fc6f6d1cd")
+                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/4.0.4/beta-18/TaboolaSDK.xcframework.zip",
+                      checksum: "0d348111b63252c1d8ea75aaea5099f5e937d8f3c84c6374d5233ca1e6c79d1f")
     ]
 )
