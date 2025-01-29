3ebee2d8cc9fd08d8e97fd7f54da1a280cb205cb
Yoni Vizel <yonivav@gmail.com>
2025-01-29 15:34:29 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/4.0.3/beta-100/TaboolaSDK.xcframework.zip",
-                      checksum: "363c4888b6ee104ab4e0e435f0381d33788a90b9091118bd9e0ecbc285c8d58a")
+                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/4.0.3/beta-6/TaboolaSDK.xcframework.zip",
+                      checksum: "fbe7efc445f375c05747a689ca099a16742ef84a18978ccfd576a578df17f277")
+
     ]
 )
