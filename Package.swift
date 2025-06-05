5abf69517d61da2128af5ce7635fd654c6d4536e
Roman Sliepko <8329266+RomanSliepko@users.noreply.github.com>
2025-06-05 13:54:33 +0300

    Update to 4.0.10-adx-beta-3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/4.0.10/adx-beta-2/TaboolaSDK.xcframework.zip",
-                      checksum: "e8b1cf53beb55e8dc336a8e3bf9621896ac8a8a9a4e420d2047ee77423f564ca")
+                      url: "https://taboola-mobile-sdk.s3.us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/beta/4.0.10/adx-beta-3/TaboolaSDK.xcframework.zip",
+                      checksum: "ce0d84df1dfe4a4c5a4096e1b385df3da5b3ae39d87d810f5f39f6d382be384d")
     ]
 )
