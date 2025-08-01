ee7b8d363107606dc281cb4a90c9533094fe99c4
builder <builder@taboola.com>
2025-08-01 12:57:11 +0100

    Update Package to version 3.8.48

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TaboolaSDK",
-                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/official/3.8.47/TaboolaSDK.xcframework.zip",
-                      checksum: "e882cc1cea334f48df5714648cb5c7fc98ed5c21b1ed9843e893c3ca9d719383")
+                      url: "https://taboola-mobile-sdk.s3-us-west-2.amazonaws.com/ios/sdk-binaries/xcframework/official/3.8.48/TaboolaSDK.xcframework.zip",
+                      checksum: "cea38a695f706cb5f64fbe8b6824ec9ddd47975f2e53cf6df58a8151fc3f0828")
     ]
 )
